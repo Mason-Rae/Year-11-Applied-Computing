4ee3a2e1e2c4792376eb55aa62490f1b94976b4d
Gamble <rae0004@copperfieldcollege.vic.edu.au>
2025-05-23 13:31:52 +1000

    Add files via upload

---
+++ SAC 2.py
@@ -0,0 +1,13 @@
+#SAC 2
+students = int(input("How many students in the class: "))
+#Gets the amount of students in the class
+for i in range(1, students+1):
+    globals()[f"name_{i}"] = input("Student's name: ")
+    #Uses a for loop to create variables to store the names
+    periods = int(input("Number of periods that week(1-5): "))
+    while periods not in range(1,5):
+        periods = int(input("Number of periods that week(1-5): "))
+    for j in range(1, periods+1):
+        globals()[f"period{j}_{i}"]= bool(input(f"Was student present for period {j}(T/F): "))
+    #Has 'Present' or 'Absent' entered for the periods
+    
